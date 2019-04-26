293f7f8b955838af1f7d03388e64ee8df89406cd
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2019-04-26 16:22:22 +0800

    base2

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,40 @@
     <v-toolbar app color="black">
       <v-toolbar-title>Mobile Team Christmas 交換禮物！！！</v-toolbar-title>
       <v-spacer></v-spacer>
-      
+      <v-btn icon dark @click="openSettings">
+        <v-icon>settings</v-icon>
+      </v-btn>
     </v-toolbar>
     <v-content>
       <v-container fluid class="pa-0">
         <router-view></router-view>
       </v-container>
     </v-content>
-    <v-footer app></v-footer>
+    <Settings ref="Settings"></Settings>
   </v-app>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Settings from '@/components/Settings.vue'; // @ is an alias to /src
+
+export default Vue.extend({
+  data() {
+    return {
+      settingsOpen: false,
+    }
+  },
+  components: {
+    Settings,
+  },
+  methods: {
+    openSettings() {
+      this.$refs.Settings.dialog = true;
+    }
+  }
+})
+</script>
+
 
 <style lang="stylus">
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="home">
     <div class="videoWrapper">
-      <iframe class="YTbg" width="560" height="315" src="https://www.youtube.com/embed/6oD1uYkbc9Y?controls=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe class="YTbg" width="560" height="315" src="https://www.youtube.com/embed/6oD1uYkbc9Y?controls=0&autoplay=1&loop=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default Vue.extend({
   name: 'home',
-  // components: {
-  //   HelloWorld,
-  // },
 });
 </script>
 
